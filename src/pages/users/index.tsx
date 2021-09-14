37cd2e9028d1513079465bfdc8781e95d11ff0b8
Gabriele Nakassima <gaby.naka@gmail.com>
2021-09-14 16:19:53 -0300

    Finalizando integração com Miragejs e ReactQuery

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,39 +1,48 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
-import Link from 'next/link';
-import { useQuery } from 'react-query';
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner, Link } from "@chakra-ui/react";
+import { GetServerSideProps } from "next";
+import NextLink from 'next/link';
+import { useState } from 'react';
 
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { getUsers, useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/mirage/queryClient";
 
-export default function UserList() {
-    const { data, isLoading, error } = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users');
-        const data = await response.json();
+type User = {
+    id: string;
+    name: string;
+    email: string;
+    createdAt: string;
+}
 
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric'
-                })
-            }
-        })
-        return users;
-    }, {
-        staleTime: 1000 * 5, // 5 sec.
-    });
+type UserListProps = {
+    users: User;
+}
+
+export default function UserList(/* { users }: UserListProps */) {
+    const [page, setPage] = useState(1);
+    const { data, isLoading, error, isFetching } = useUsers(page, 
+        // { initialData: users }
+    );
 
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
     });
+
+    async function handlePrefetchUser(userId: string) {
+        await queryClient.prefetchQuery(['user', userId], async () => {
+            const response = await api.get(`users/${userId}`)
+
+            return response.data;
+        }, {
+            staleTime: 1000 * 60 * 10, // 10 min
+        })
+    }
 
     return (
         <Box>
@@ -44,8 +53,12 @@ export default function UserList() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
-                        <Link href="/users/create" passHref>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+                        </Heading>
+                        <NextLink href="/users/create" passHref>
                             <Button
                                 as="a"
                                 size="sm"
@@ -55,7 +68,7 @@ export default function UserList() {
                             >
                                 Criar novo
                             </Button>
-                        </Link>
+                        </NextLink>
                     </Flex>
 
                     {isLoading ? (
@@ -80,7 +93,7 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    {data.map(user => {
+                                    {data.users.map(user => {
                                         return (
                                             <Tr key={user.id}>
                                                 <Td px={["4", "4", "6"]}>
@@ -88,7 +101,9 @@ export default function UserList() {
                                                 </Td>
                                                 <Td>
                                                     <Box>
-                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                                                            <Text fontWeight="bold">{user.name}</Text>
+                                                        </Link>
                                                         <Text fontSize="sm" color="gray.300">{user.email}</Text>
                                                     </Box>
                                                 </Td>
@@ -111,7 +126,11 @@ export default function UserList() {
                                 </Tbody>
                             </Table >
 
-                            <Pagination />
+                            <Pagination
+                                totalCountOfRegisters={data.totalCount}
+                                currentPage={page}
+                                onPageChange={setPage}
+                            />
                         </>
                     )
                     }
@@ -120,3 +139,13 @@ export default function UserList() {
         </Box >
     )
 }
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//     const { users } = await getUsers(1);
+
+//     return {
+//         props: {
+//             users,
+//         }
+//     }
+// }
